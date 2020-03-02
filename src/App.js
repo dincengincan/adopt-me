f3401760dbc2d877bc7bf6d19c270c3a99f268fa
dincengincan <engincandinc@gmail.com>
2020-03-02 23:25:12 +0300

    add react components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import Pet from './Pet'
+
 
 const App = () => {
     return (
         <div>
-            <h1>HELLO</h1>
+            <h1>Adopt me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanees" />
+            <Pet name="Pepper" animal="Bird" breed="Cockatail" />
+            <Pet name="Doink" animal="Cat" breed="Mixed" />
         </div>
     )
 }
