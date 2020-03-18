89b81d2b1a1f07f24b62cf8d5abbdeb93ee23287
dincengincan <engincandinc@gmail.com>
2020-03-18 22:18:10 +0300

    add useDropdown custom Hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import SearchParams from "./SearchParams"
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Adopt me!</h1>
-            <SearchParams />
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
 
 render(React.createElement(App), document.getElementById("root"));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
-    if (some) {
-        const [location, setLocation] = useState("Seattle, WA");
-    }
-
-
-
-    return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    location
-                    <input id="location" value={location} placeholder="location" onChange={e => setLocation(e.target.value)} />
-                    <button>Submit</button>
-                </label>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={e => setLocation(e.target.value)}
+          />
+          <button>Submit</button>
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchParams;
